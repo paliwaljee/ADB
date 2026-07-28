5411e35f7340ff14c6f79d4503303466972ac4bd
Lalit Paliwal <Lalit.paliwal989@gmail.com>
2026-07-28 20:06:22 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/YAADU/",
+  base: "/ADB/",
   assetsInclude: ["**/*.lottie"],
   build: {
     target: "esnext",
